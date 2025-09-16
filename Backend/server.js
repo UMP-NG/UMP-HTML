badc35fd0d03e98bc82cc3f1f269fd3730a70cd2
Techtide0 <techtideenterprise0@gmail.com>
2025-09-16 21:47:37 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   "http://127.0.0.1:5500", 
   "http://localhost:5500",
-  "https://ump-ng.github.io/UMP-HTML"
+  "https://ump-ng.github.io"
 ];
 
 app.use(cors({
