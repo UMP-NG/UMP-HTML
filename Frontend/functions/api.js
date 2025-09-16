663b4743b539571d03a479f869f61105813711c2
Techtide0 <techtideenterprise0@gmail.com>
2025-09-16 20:31:54 +0100

    Update api.js

--- Frontend/functions/api.js
+++ Frontend/functions/api.js
@@ -1,5 +1,5 @@
 // js/api.js
-const API_BASE = "https://ump-html-1.onrender.com";
+const API_BASE = "https://ump-html-1.onrender.com/api/auth/login";
 
 async function apiFetch(path, options = {}) {
   const headers = options.headers || {};
